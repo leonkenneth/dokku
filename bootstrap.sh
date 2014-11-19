2c65e9f53d6e7ab266081ffba60635edcb66935d
Léo-Paul Goffic <leo@hublo.co>
2014-11-19 08:57:33 +0100

    WIP

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,6 @@ set -eo pipefail
 export DEBIAN_FRONTEND=noninteractive
 export DOKKU_REPO=${DOKKU_REPO:-"https://github.com/leonkenneth/dokku.git"}
 export DOKKU_BRANCH=${DOKKU_BRANCH:-"feature/dind"}
-export DOCKER_HOST="tcp://0.0.0.0:2375"
 
 
 if ! which apt-get &>/dev/null
@@ -17,15 +16,15 @@ apt-get install -qq -y git make curl software-properties-common man-db
 
 [[ `lsb_release -sr` == "12.04" ]] && apt-get install -qq -y python-software-properties
 
-cd ~ && test -d dokku || git clone $DOKKU_REPO
-cd dokku
-git fetch origin
-
-if [[ -n $DOKKU_BRANCH ]]; then
-  git checkout origin/$DOKKU_BRANCH
-elif [[ -n $DOKKU_TAG ]]; then
-  git checkout $DOKKU_TAG
-fi
+# cd ~ && test -d dokku || git clone $DOKKU_REPO
+# cd dokku
+# git fetch origin
+#
+# if [[ -n $DOKKU_BRANCH ]]; then
+#   git checkout origin/$DOKKU_BRANCH
+# elif [[ -n $DOKKU_TAG ]]; then
+#   git checkout $DOKKU_TAG
+# fi
 
 make install
 
